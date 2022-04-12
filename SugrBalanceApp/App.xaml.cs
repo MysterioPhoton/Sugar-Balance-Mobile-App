fdf60b3c82aa58759f7f30968d9b9a49fc53d64e
Puja Biswas (biswasp3) <biswasp3@coventry.ac.uk>
2022-04-12 15:42:27 +0100

    Add files via upload
    error solved

---
+++ SugrBalanceApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SugrBalance_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new RegPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
