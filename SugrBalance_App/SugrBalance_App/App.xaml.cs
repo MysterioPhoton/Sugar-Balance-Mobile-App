dd76b4071cb78a082985207602679e9325a90396
Puja Biswas (biswasp3) <biswasp3@coventry.ac.uk>
2022-04-19 15:43:02 +0100

    Add files via upload

--- SugrBalance_App/SugrBalance_App/App.xaml.cs
+++ SugrBalance_App/SugrBalance_App/App.xaml.cs
@@ -1,1 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+namespace SugrBalance_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        public class GlobalVars
+        {
+            public static string userEmail = "";
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
